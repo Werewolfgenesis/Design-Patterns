978b2da99815f989f523e3cc2e58160167fde7fa
Dimitar Sotirov <64849383+Werewolfgenesis@users.noreply.github.com>
2020-11-04 20:45:50 +0200

    Update design_patterns.cpp

--- design_patterns.cpp
+++ design_patterns.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
 //singleton 
+template <class T>
+class Singleton
+{
+private:
+    static Singleton* instance;
+    T data;
+
+    //default constructor is private so no objects of type Singleton can be created
+    Singleton()
+    {
+        this->data = 0;
+    }
+public:
+    static Singleton* getInstance()
+    {
+        if (!(this->instance))
+        {
+            this->instance = new Singleton;
+        }
+        return this->instance;
+    }
+    const T& get_data()
+    {
+        return this->data;
+    }
+    void set_data(const T& update)
+    {
+        this->data = update;
+    }
+};
 
 int main()
 {
